01da8b689db190aa9ff446b4a1fcd059b6060778
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2021-09-15 16:12:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char** argv) {
     Engine engine = Engine();
-    if (!engine.start("^_^", 800, 600)) {
+    if (!engine.start("^_^", 1024, 720)) {
         printf("Error starting 3D engine\n");
     } else {
         printf("Ended start engine task\n");
