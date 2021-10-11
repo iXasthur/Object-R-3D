ed98a153771ade4ba21e58a30f428bd57e40cde5
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2021-10-11 21:35:38 +0300

    Update Scene.h

--- engine/scene/Scene.h
+++ engine/scene/Scene.h
@@ -19,7 +19,7 @@ public:
     std::vector<Object> objects{};
     Camera camera = Camera({0, 0, 0}, {0, 0, 0});
     Light light = Light(
-            {1.0f, 0.0f, 1.0f}, // screen space
+            {1.0f, 0.0f, 1.0f},
             {0.05f, {255, 255, 255, 255}},
             {1.0f, {255, 255, 255, 255}},
             {}
